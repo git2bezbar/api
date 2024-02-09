f558019ee4e1b895da0727d176cc7f769631a68d
Adem <ademduran6888@gmail.com>
2024-02-09 15:19:14 +0100

    feat(routes): making fake data returns for API tests

--- start/routes.ts
+++ start/routes.ts
@@ -14,3 +14,140 @@ router.get('/', async () => {
     hello: 'world',
   }
 })
+
+/**
+ * Contact Settings
+ */
+
+router.group(() => {
+  router.get('/contact-settings', async () => {
+    return {
+      email: 'api@forkee.site',
+      phone_number: '0359741029',
+      social_links: [
+        {
+          platform: 'facebook',
+          url: 'facebook.com/forkee',
+          is_active: true,
+        },
+        {
+          platform: 'twitter',
+          url: 'twitter.com/forkee',
+          is_active: false,
+        },
+        {
+          platform: 'instagram',
+          url: 'instagram.com/forkee',
+          is_active: true,
+        },
+        {
+          platform: 'thefork',
+          url: 'thefork.fr/forkee',
+          is_active: false,
+        },
+        {
+          platform: 'tripadvisor',
+          url: 'tripadvisor.com/forkee',
+          is_active: false,
+        },
+      ],
+    }
+  })
+})
+
+/**
+ * General Settings
+ */
+
+router.group(() => {
+  router.get('/general-settings', async () => {
+    return {
+      website_title: 'Forkee',
+      website_headline: 'The best or nothing',
+      website_logo: {
+        filename: 'forkee-logo.png',
+        path: '/img/forkee-logo.png',
+        description: 'Forkee logo',
+      },
+      website_favicon: {
+        filename: 'forkee-favicon.png',
+        path: '/img/forkee-favicon.png',
+        description: 'Forkee favicon',
+      },
+      address: {
+        street: '46 Sente des Radoubs',
+        post_code: '33300',
+        city: 'Bordeaux',
+      },
+      timetable: [
+        {
+          day_of_week: 'Lundi',
+          opening_time: '2002-02-21T08:30:00.000Z',
+          closing_time: '2002-02-21T12:30:00.000Z',
+        },
+        {
+          day_of_week: 'Lundi',
+          opening_time: '2002-02-21T14:30:00.000Z',
+          closing_time: '2002-02-21T17:30:00.000Z',
+        },
+        {
+          day_of_week: 'Mardi',
+          opening_time: '2002-02-21T08:30:00.000Z',
+          closing_time: '2002-02-21T12:30:00.000Z',
+        },
+        {
+          day_of_week: 'Mardi',
+          opening_time: '2002-02-21T14:30:00.000Z',
+          closing_time: '2002-02-21T17:30:00.000Z',
+        },
+        {
+          day_of_week: 'Mercredi',
+          opening_time: '2002-02-21T08:30:00.000Z',
+          closing_time: '2002-02-21T12:30:00.000Z',
+        },
+        {
+          day_of_week: 'Mercredi',
+          opening_time: '2002-02-21T14:30:00.000Z',
+          closing_time: '2002-02-21T17:30:00.000Z',
+        },
+      ],
+    }
+  })
+})
+
+/**
+ * Website Customization
+ */
+
+router.group(() => {
+  router.get('/customization', async () => {
+    return {
+      header_layout: 1,
+      theme: 2,
+      primary_color: '#FF9900',
+      secondary_color: '#6624FF',
+      title_font: 'Poppins',
+      text_font: 'DM Sans',
+      button_font: 'Raleway',
+      footer_layout: 3,
+    }
+  })
+})
+
+/**
+ * Account
+ */
+
+router.group(() => {
+  router.get('/account', async () => {
+    return {
+      firstname: 'John',
+      lastname: 'Doe',
+      profile_picture: {
+        filename: 'profile-picture.png',
+        path: '/img/profile-picture.png',
+        description: 'Profile Picture',
+      },
+    }
+  })
+})
