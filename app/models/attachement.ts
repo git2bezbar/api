ef52d054ac9685d94c5bab103d576052874e8e20
Adem <ademduran6888@gmail.com>
2024-02-11 00:09:56 +0100

    fix(models): correcting models

--- app/models/attachement.ts
+++ app/models/attachement.ts
@@ -1,28 +1,25 @@
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 import { belongsTo, column } from '@adonisjs/lucid/orm'
+
 import BasicModel from './base.js'
-import Category from './category.js'
 import Website from './website.js'
 
-export default class Product extends BasicModel {
+export default class Attachement extends BasicModel {
   @column()
-  declare name: string
+  declare filename: string
+
+  @column()
+  declare path: string
 
   @column()
   declare description: string
 
   @column()
-  declare price: number
-
-  @column()
-  declare category_id: number
-
-  @belongsTo(() => Category)
-  declare category: BelongsTo<typeof Category>
+  declare type: string
 
   @column()
   declare website_id: number
 
   @belongsTo(() => Website)
-  declare website: BelongsTo<typeof Website>
+  declare category: BelongsTo<typeof Website>
 }
--- app/models/widget.ts
+++ app/models/widget.ts
@@ -1,13 +1,22 @@
-import { column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { belongsTo, column } from '@adonisjs/lucid/orm'
+
 import BasicModel from './base.js'
+import Page from './page.js'
 
 export default class Widget extends BasicModel {
   @column()
   declare name: string
 
   @column()
-  declare content: JSON
+  declare content: object
 
   @column()
   declare order: number
+
+  @column()
+  declare page_id: number
+
+  @belongsTo(() => Page)
+  declare category: BelongsTo<typeof Page>
 }
