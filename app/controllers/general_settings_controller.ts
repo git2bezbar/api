658e408b2283209bc4409290a54ea3defa63a515
Adem <ademduran6888@gmail.com>
2024-02-20 10:05:09 +0100

    feat(controller): adding the index and update methods to the general settings controller

---
+++ app/controllers/general_settings_controller.ts
@@ -0,0 +1,71 @@
+import TimeSlot from '#models/time_slot'
+import Website from '#models/website'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class GeneralSettingsController {
+  async show({ params }: HttpContext) {
+    const { uuid } = params
+    const {
+      websiteTitle,
+      websiteHeadline,
+      websiteLogo,
+      websiteFavicon,
+      street,
+      postCode,
+      city,
+      timeSlots,
+    } = await Website.query().where('uuid', uuid).preload('timeSlots').firstOrFail()
+    const generalSettings = {
+      websiteTitle,
+      websiteHeadline,
+      websiteLogo,
+      websiteFavicon,
+      street,
+      postCode,
+      city,
+      timeSlots,
+    }
+
+    return generalSettings
+  }
+
+  async update({ request, params }: HttpContext) {
+    const { uuid } = params
+    const {
+      websiteTitle: updatedWebsiteTitle,
+      websiteHeadline: updatedWebsiteHeadline,
+      websiteLogo: updatedWebsiteLogo,
+      websiteFavicon: updatedWebsiteFavicon,
+      street: updatedStreet,
+      postCode: updatedPostCode,
+      city: updatedCity,
+      timeSlots: updatedTimeSlots,
+    } = request.all()
+    const { id } = await Website.query().where('uuid', uuid).firstOrFail()
+    const updatedGeneralSettings = await Website.query().where('uuid', uuid).update({
+      websiteTitle: updatedWebsiteTitle,
+      websiteHeadline: updatedWebsiteHeadline,
+      websiteLogo: updatedWebsiteLogo,
+      websiteFavicon: updatedWebsiteFavicon,
+      street: updatedStreet,
+      postCode: updatedPostCode,
+      city: updatedCity,
+    })
+
+    if (updatedTimeSlots.length) {
+      updatedTimeSlots.map(async (timeSlot: TimeSlot) => {
+        console.log(id, timeSlot, timeSlot.dayOfWeek, timeSlot.slotNumber)
+        await TimeSlot.query()
+          .where('websiteId', id)
+          .where('dayOfWeek', timeSlot.dayOfWeek)
+          // fix that shit
+          .where('slotNumber', timeSlot.slot_number)
+          .update({
+            openingTime: timeSlot.openingTime,
+            closingTime: timeSlot.closingTime,
+          })
+      })
+    }
+    return updatedGeneralSettings
+  }
+}
