69118a396151a665d10fa8e32c75b4b097f38a32
Adem <ademduran6888@gmail.com>
2024-03-24 13:52:44 +0100

    feat(routes): adding auth middleware, auth routes and account routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,12 +7,15 @@
 |
 */
 
+const AuthController = () => import('#controllers/auth_controller')
+import { middleware } from '#start/kernel'
+import router from '@adonisjs/core/services/router'
+
 const ContactSettingsController = () => import('#controllers/contact_settings_controller')
 const GeneralSettingsController = () => import('#controllers/general_settings_controller')
 const CustomizationsController = () => import('#controllers/customizations_controller')
 const PagesController = () => import('#controllers/pages_controller')
 const MenusController = () => import('#controllers/menus_controller')
-import router from '@adonisjs/core/services/router'
 
 router
   .group(() => {
@@ -63,3 +66,19 @@ router
     })
   })
   .prefix('/:uuid')
+  .use(middleware.auth())
+
+router
+  .group(() => {
+    router.post('/login', [AuthController, 'store'])
+    router.get('/me', [AuthController, 'check'])
+    router.post('/logout', [AuthController, 'logout'])
+  })
+  .prefix('/auth')
+
+router
+  .group(() => {
+    router.get('/account', [AuthController, 'index'])
+    router.post('/account', [AuthController, 'update'])
+  })
+  .use(middleware.auth())
