ef03238ca228d0d514df06fc34b2cbc29ceb3d8a
Adem <ademduran6888@gmail.com>
2024-03-24 13:52:19 +0100

    fix(config): import the correct model in the auth config

--- config/auth.ts
+++ config/auth.ts
@@ -8,7 +8,7 @@ const authConfig = defineConfig({
     web: sessionGuard({
       useRememberMeTokens: false,
       provider: sessionUserProvider({
-        model: () => import('#models/_user'),
+        model: () => import('#models/user'),
       }),
     }),
   },
